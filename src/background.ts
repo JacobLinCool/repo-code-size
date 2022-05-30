fd77d1d09a09e21a954c4d5d6ab9049751f028a4
JacobLinCool <jacoblincool@gmail.com>
2022-05-30 12:54:31 +0800

    fix: service worker inactive

--- src/background.ts
+++ src/background.ts
@@ -2,29 +2,23 @@ import { log } from "./utils";
 
 type Action = "get" | "set";
 
-chrome.runtime.onInstalled.addListener(load);
+chrome.runtime.onMessage.addListener(function (request, sender, respond) {
+    const action: Action = request.action;
 
-function load() {
-    chrome.runtime.onMessage.addListener(function (request, sender, respond) {
-        const action: Action = request.action;
+    log({ action });
 
-        log({ action });
+    if (action === "get" && (is_popup(sender) || is_github(sender))) {
+        chrome.storage.sync.get("token", (result) => {
+            respond({ token: result.token || "" });
+        });
+    } else if (action === "set" && is_popup(sender)) {
+        chrome.storage.sync.set({ token: request.token || "" }).then(() => {
+            respond({});
+        });
+    }
 
-        if (action === "get" && (is_popup(sender) || is_github(sender))) {
-            chrome.storage.sync.get("token", (result) => {
-                respond({ token: result.token || "" });
-            });
-        } else if (action === "set" && is_popup(sender)) {
-            chrome.storage.sync.set({ token: request.token || "" }).then(() => {
-                respond({});
-            });
-        }
-
-        return true;
-    });
-
-    log("service is running");
-}
+    return true;
+});
 
 function is_popup(sender: chrome.runtime.MessageSender): boolean {
     return !sender.tab;
